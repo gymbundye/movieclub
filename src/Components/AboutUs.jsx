e3da0d00807c99d28e62b8d6de5ed39aa7929b00
gymbundye <gymbundye@gmail.com>
2022-03-22 11:11:47 -0400

    started to build real movie club

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+
+function AboutUs() {
+  return (
+      
+    <div>
+        <Header/>
+
+
+
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Movie Club Society</div>
+  )
+}
+
+export default Header
