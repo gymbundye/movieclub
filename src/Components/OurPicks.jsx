707d90a96c3edc88c21e2e9677c48147a0a180f9
gymbundye <gymbundye@gmail.com>
2022-07-05 17:43:48 -0400

    working on our picks

--- src/Components/OurPicks.jsx
+++ src/Components/OurPicks.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function OurPicks() {
   return (
-    <div>OurPicks</div>
+    <div>
+      <div className='about'><center>This is a complete list of all the movies that movie club has done!</center></div>
+    <div className='about'>List PLace Holder</div>
+    </div>
   )
 }
 
