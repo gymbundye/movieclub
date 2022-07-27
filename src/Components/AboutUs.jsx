a28857484fbced28f8ee8bdfce9a23122d66541f
gymbundye <gymbundye@gmail.com>
2022-07-26 23:06:01 -0400

    worked on color scheme and copy

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -11,11 +11,11 @@ function AboutUs() {
         <p className='about'><center>Who are the Sons  of the Marianne?</center><br></br>
        
          We LOVE movies. Who doesn't. But during the dark times of the Pandemic my two friends and I could no longer go out and enjoy
-          going to the movies in the same way. New movies were not coming out, and if they 
-          did it was home release only.  We came up with a fun way to still enjoy movies and scratch that itch we were missing in  our lives. 
-          The gist of "Movie Club Society" is simple. Every week we take a turn picking a movie. We watch it seperatley through out the week 
-          whenever we can find the time. We meet on Mondays to talk about it. Simple. We have seen alot of cool movies this way. 
-          We have seen a lot of garbage movies this way. Part of the fun of the club is that you never know what you are going to get! </p>
+         going to the movies in the same way. New movies were not coming out, and if they 
+         did it was home release only.  We came up with a fun way to still enjoy movies and scratch that itch we were missing in  our lives. 
+         The gist of "Sons of the Marianne" is simple. Every week we take a turn picking a movie. We watch it seperatley through out the week 
+         whenever we can find the time. We meet on Mondays to talk about it. Simple. We have seen alot of cool movies this way. 
+         We have seen a lot of garbage movies this way. Part of the fun of the club is that you never know what you are going to get! </p>
     
     <div className='who'><center> Who are we?</center>
     <img src="/images/3d icon.png" alt="3d glasses" width={"100"}/>
@@ -25,13 +25,13 @@ function AboutUs() {
     <div className='rules'>Rules:
       
         <ul title='Rules'>
-          <li><i>Do  </i>talk about Movie Club Society! Pick a day of the week for your clubs meet!</li>
+          <li>Pick a day of the week for your clubs meet!</li>
           <li>You alternate movie picks.</li>
           <li>Even if you have seen the movie before you must watch it during its week.</li>
           <li>Every club decides one movie that may NEVER be watched (ours is that all girl Ghostbusters movie).</li>
           <li>During the month of October it has to be a scary movie.</li>
           <li>During the month of December it has to be an X-mas movie.</li>
-          <li>If it is your Birthday month you pick a movie for all to watch.</li>
+          <li>If it is your Birthday month you pick an extra movie for all to watch.</li>
         </ul>
       </div>
     
